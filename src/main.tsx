ac6a8a12c2e31d7eedca9f03714026eeb75d28a2
Benish khan <141191349+boxing-bee@users.noreply.github.com>
2025-04-04 10:27:25 -0700

    Update tailwind config with darkmode settings

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import * as LDClient from 'launchdarkly-js-client-sdk';
+const options: LDClient.LDOptions = { allAttributesPrivate: true };
+const client = LDClient.initialize('67ef2147c1c6dc09860736ad', context, options);
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class', //adding in tailwind configuration for darkmode part.1
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
