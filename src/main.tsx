fc11cdb0b88fdbb01496206b26d098e54dd24e9d
Benish khan <141191349+boxing-bee@users.noreply.github.com>
2025-04-04 12:14:13 -0700

    Updated code to troubleshoot skd initialization issues

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,28 @@
 import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/LDLDClient';
+import { createRoot } from 'react-dom/client';
 import { useState, useEffect } from 'react';
 import App from './App.tsx';
 import './index.css';
-import * as LDClient from 'launchdarkly-js-LDLDClient-sdk';
+import * as LaunchDarkly from 'launchdarkly-js-client-sdk';
 
-const context: LDClient.LDContext = {
+const context: LaunchDarkly.LDContext = {
   kind: 'user',
   key: 'beta-users', // Replace with a unique identifier for the user
 };
 
-const options: LDClient.LDOptions = { allAttributesPrivate: true };
-const LDLDClient = LDClient.initialize('67ef2147c1c6dc09860736ad', context, options);
+const options: LaunchDarkly.LDOptions = { allAttributesPrivate: true };
+const LDClient = LaunchDarkly.initialize('67ef2147c1c6dc09860736ad', context, options);
 
 function MainApp() {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   useEffect(() => {
-    LDLDClient.on('ready', () => {
-      const initialFlagValue = LDLDClient.variation('dark-mode', false);
+    LDClient.on('ready', () => {
+      const initialFlagValue = LDClient.variation('dark-mode', false);
       setIsDarkMode(initialFlagValue);
 
       // Listener to update when flag changes
-      LDLDClient.on('change:dark-mode', (current) => {
+      LDClient.on('change:dark-mode', (current) => {
         console.log('Flag "dark-mode" changed to:', current);
         setIsDarkMode(current);
       });
