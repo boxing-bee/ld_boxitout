831780df05b48ffa03815e568ecac6266a1e03c6
Benish khan <141191349+boxing-bee@users.noreply.github.com>
2025-04-04 21:14:53 -0700

    Commented out context syntax

--- src/client.js
+++ src/client.js
@@ -1,11 +1,11 @@
 import * as LDClient from 'launchdarkly-js-client-sdk';
 
 // You'll need this context later, but you can ignore it for now.
-const context: LDClient.LDContext = {
-    kind: 'user',
-    key: 'beta-user',
-    name: 'Betty',
-    email: 'betty@testcorp.com'
+// const context: LDClient.LDContext = {
+//     kind: 'beta-user',
+//     key: 'fill-in',
+//     name: 'Betty',
+//     email: 'betty@testcorp.com'
 };
 const client = LDClient.initialize('67ef2147c1c6dc09860736ad', context);
 
